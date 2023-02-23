f37e75303312ccf41f6f7fef3f57f8f8b8ff73a7
jino.michel.aque <contact@geeklogbook.com>
2023-02-23 08:25:08 -0300

    add rest of the project

--- btre/settings.py
+++ btre/settings.py
@@ -116,7 +116,5 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'btre/static') 
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
